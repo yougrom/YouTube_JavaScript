5d69fe2bb22d237de8e5d09afec39c21376eddb3
Herr Gromov <youriy.gromov@gmail.com>
2022-06-23 17:44:42 +0200

    practice with variable exercises

---
+++ practisVarEight.js
@@ -0,0 +1,11 @@
+// 8 Практика по объявлению переменных
+let myName
+console.log(myName);
+
+myName = 'Yuriy'
+console.log(myName);// 8 Практика по объявлению переменных
+let myName
+console.log(myName);
+
+myName = 'Yuriy'
+console.log(myName);
